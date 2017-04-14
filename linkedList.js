43662ae2b965a8de02bcd4a61696d6b2dde373e8
ADL175 <armydavidlim@gmail.com>
2017-04-13 17:06:03 -0700

    list display method added

--- linkedList.js
+++ linkedList.js
@@ -1,27 +1,56 @@
 'use strict';
-// //////////////////////////////////////////////
-// LINKEDLIST DATA STRUCTURE CONSTRUCTOR //
-// //////////////////////////////////////////////
 
-function Node(value, next) {
+function Node (value, next){
   this.value = value;
-  this.next = next ? next : null;
-}
+  this.next = next;
+};
+
+// [5] -> [6] -> [3] -> [18]
+// ['apple'] -> ['turkey'] -> ['pie'] -> ['pb&j']
 
 function LinkedList(){
   this.head = null;
 }
 
-LinkedList.prototype.push = function(val){
-  this.head = new Node(val, this.head);
+LinkedList.prototype.push = function(value){
+  this.head = new Node(value, this.head);
 };
+
+// LinkedList.prototype.display = function(){
+//   returns list.head.value, list.head.next.value;
+// };
+  // visualize whatever data is in your linked list as a string.
+  // ex: [5] -> [6] -> [3] -> [18]
 
 var list = new LinkedList();
 
-list.push(9);
-list.push(19);
-list.push(29);
-list.push(39);
-list.push(49);
+list.push(18);
+list.push(3);
+list.push(6);
+list.push(5);
+list.push('poop');
+list.push('fuck');
+console.log(list.head);
 
-console.log(list.head.next);;
+  /*l
+  list.display() returns '[5] -> [6] -> [3] -> [18]'
+
+  Also implement the "length" attribute.
+  The length attribute is a property of the constructor.
+  An empty linked list has 0 length.
+  The length increases whenever you push a new value on.
+  */
+// };
+
+LinkedList.prototype.display = function(){
+  var p = this.head;
+  var listThing = '';
+  while (p){
+    listThing += `[${p.value}] -> `;
+    p = p.next;
+
+  }
+  return listThing;
+};
+
+list.display();
